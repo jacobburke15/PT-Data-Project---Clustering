f048ff644996b990fee735b8170fa6bb742aac46
Jacob Burke <jacobburke15@gmail.com>
2020-04-19 19:38:11 -0400

    k

---
+++ autoencoder_example.R
@@ -0,0 +1,142 @@
+
+## installing the keras package needed 
+
+## devtools::install_github("rstudio/keras")
+
+library(keras)
+install_keras()
+
+K <- keras::backend()
+
+# Parameters (batch size, epochs, the start dimensions, intermediate and end dimensions of autoencoder
+
+batch_size <- 100L
+original_dim <- 784L
+latent_dim <- 2L
+intermediate_dim <- 256L
+epochs <- 50L
+epsilon_std <- 1.0
+
+# Model definition (has an original, intermediate, and latent dim) --------------------------------------------------------
+
+x <- layer_input(shape = c(original_dim))
+h <- layer_dense(x, intermediate_dim, activation = "relu")
+z_mean <- layer_dense(h, latent_dim)
+z_log_var <- layer_dense(h, latent_dim)
+
+sampling <- function(arg){
+  z_mean <- arg[, 1:(latent_dim)]
+  z_log_var <- arg[, (latent_dim + 1):(2 * latent_dim)]
+  
+  epsilon <- k_random_normal(
+    shape = c(k_shape(z_mean)[[1]]), 
+    mean=0.,
+    stddev=epsilon_std
+  )
+  
+  z_mean + k_exp(z_log_var/2)*epsilon
+}
+
+## z here is the latent data condensed from the x data 
+
+z <- layer_concatenate(list(z_mean, z_log_var)) %>% 
+  layer_lambda(sampling)
+
+# we instantiate these layers separately so as to reuse them later 
+
+## ***(may not need this decoder section once encoded for our PT data)***
+
+decoder_h <- layer_dense(units = intermediate_dim, activation = "relu")
+decoder_mean <- layer_dense(units = original_dim, activation = "sigmoid")
+h_decoded <- decoder_h(z)
+x_decoded_mean <- decoder_mean(h_decoded)
+
+# end-to-end autoencoder
+vae <- keras_model(x, x_decoded_mean)
+
+# encoder, from inputs to latent space
+
+encoder <- keras_model(x, z_mean)
+
+# generator, from latent space to reconstructed inputs (ie. decoder)
+decoder_input <- layer_input(shape = latent_dim)
+h_decoded_2 <- decoder_h(decoder_input)
+x_decoded_mean_2 <- decoder_mean(h_decoded_2)
+generator <- keras_model(decoder_input, x_decoded_mean_2)
+
+
+## loss function that is incorporated in Variational autoencoders 
+
+vae_loss <- function(x, x_decoded_mean){
+  xent_loss <- (original_dim/1.0)*loss_binary_crossentropy(x, x_decoded_mean)
+  kl_loss <- -0.5*k_mean(1 + z_log_var - k_square(z_mean) - k_exp(z_log_var), axis = -1L)
+  xent_loss + kl_loss
+}
+
+vae %>% compile(optimizer = "rmsprop", loss = vae_loss, experimental_run_tf_function = FALSE)
+
+
+# Data preparation --------------------------------------------------------
+
+mnist <- dataset_mnist()
+x_train <- mnist$train$x/255
+x_test <- mnist$test$x/255
+x_train <- array_reshape(x_train, c(nrow(x_train), 784), order = "F")
+x_test <- array_reshape(x_test, c(nrow(x_test), 784), order = "F")
+
+
+# Model training ----------------------------------------------------------
+
+## this is the end to end autoencoder 
+
+vae %>% fit(
+  x_train, x_train, 
+  shuffle = TRUE, 
+  epochs = epochs, 
+  batch_size = batch_size,
+  validation_data = list(x_test, x_test), 
+  
+  
+)
+
+
+# Visualizations ----------------------------------------------------------
+
+## visualizing the encoded data
+
+library(ggplot2)
+library(dplyr)
+
+x_test_encoded <- predict(encoder, x_test, batch_size = batch_size) ## passing in the encoder model with the test data
+
+## plotting the encoded 2-deminsions, we will need to do this with our data once encoded
+
+x_test_encoded %>%
+  as_data_frame() %>% 
+  mutate(class = as.factor(mnist$test$y)) %>%
+  ggplot(aes(x = V1, y = V2, colour = class)) + geom_point()
+
+# display a 2D manifold of the digits
+n <- 15  # figure with 15x15 digits
+digit_size <- 28
+
+# display a 2D manifold of the digits
+n <- 15  # figure with 15x15 digits
+digit_size <- 28
+
+# we will sample n points within [-4, 4] standard deviations
+
+#### this section is now plotting the decoded data (generator = decoder function)
+grid_x <- seq(-4, 4, length.out = n)
+grid_y <- seq(-4, 4, length.out = n)
+
+rows <- NULL
+for(i in 1:length(grid_x)){
+  column <- NULL
+  for(j in 1:length(grid_y)){
+    z_sample <- matrix(c(grid_x[i], grid_y[j]), ncol = 2)
+    column <- rbind(column, predict(generator, z_sample) %>% matrix(ncol = 28) )
+  }
+  rows <- cbind(rows, column)
+}
+rows %>% as.raster() %>% plot()
